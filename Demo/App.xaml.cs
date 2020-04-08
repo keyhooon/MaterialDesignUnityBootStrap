29b899f39252824cefd41293f8b21a6445831f8e
keyhanbabazadeh <keyhanbabazadeh@hotmail.com>
2020-04-08 12:52:39 +0430

    ...

--- Demo/App.xaml.cs
+++ Demo/App.xaml.cs
@@ -6,6 +6,7 @@ using Demo.Views;
 using MaterialDesignThemes.Wpf;
 using Prism.Ioc;
 using Prism.Regions;
+using System;
 using System.Collections.ObjectModel;
 using System.Windows;
 
@@ -18,9 +19,6 @@ namespace Demo
     {
         protected override Window CreateShell()
         {
-            Container.Resolve<IRegionManager>().RegisterViewWithRegion("Tools", typeof(ToolView));
-            Container.Resolve<IRegionManager>().RegisterViewWithRegion("ToolBar", typeof(ToolBarView));
-
             return base.CreateShell();
         }
         protected override void OnInitialized()
@@ -30,7 +28,7 @@ namespace Demo
             var compositeMapNavigatorService = Container.Resolve<CompositeMapNavigatorService>();
 
             compositeMapNavigatorService.RegisterItem("Cardio", MapItemBuilder.CreateDefaultBuilder("Cardio").WithImagePackIcon(PackIconKind.Heart).WithChild(new Collection<MapItem> {
-                    compositeMapNavigatorService.RegisterItem("CardioSignal",MapItemBuilder.CreateDefaultBuilder("Signal").WithView(typeof(Content1View)).WithImagePackIcon(PackIconKind.Signal)),
+                    compositeMapNavigatorService.RegisterItem("CardioSignal",MapItemBuilder.CreateDefaultBuilder("Signal").WithToolbars(new [] { typeof(ToolBarView) }).WithView(typeof(Content1View)).WithImagePackIcon(PackIconKind.Signal)),
                     compositeMapNavigatorService.RegisterItem("CardioAnalysis",MapItemBuilder.CreateDefaultBuilder("Analysis").WithView(typeof(Content2View)).WithImagePackIcon(PackIconKind.Analog))
                 }));
         }
