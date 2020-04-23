7e2c26d9af2dbe7074e8d66c804151a6de009089
keyhanbabazadeh <keyhanbabazadeh@hotmail.com>
2020-04-23 17:02:44 +0430

    ...

---
+++ MaterialDesignUnityBootStrap.SharpCommunication/Services/ContainerExtension.cs
@@ -0,0 +1,40 @@
+﻿using Communication.Codec;
+using Prism.Ioc;
+using SharpCommunication.Base.Channels;
+using SharpCommunication.Base.Codec;
+using SharpCommunication.Base.Transport;
+using SharpCommunication.Base.Transport.SerialPort;
+
+namespace Communication
+{
+    public static class ContainerExtension
+    {
+        public static IContainerRegistry UseCommunication(this IContainerRegistry containerRegistry)
+        {
+            return containerRegistry.Register<ICodec<DevicePacket>, DevicePacketCodec>()
+            .Register<IChannelFactory<DevicePacket>, ChannelFactory<DevicePacket>>()
+            .Register<DataTransport<DevicePacket>, SerialPortDataTransport<DevicePacket>>()
+            .RegisterInstance(new SerialPortDataTransportOption("com6", 115200))
+            .RegisterSingleton<DataTransportFacade>();
+        }
+        public static IContainerRegistry UseCodec(this IContainerRegistry containerRegistry)
+        {
+            return containerRegistry
+                 .RegisterInstance(BatteryConfigurationEncoding.CreateBuilder())
+                 .RegisterInstance(BatteryOutputEncoding.CreateBuilder())
+                 .RegisterInstance(CoreConfigurationEncoding.CreateBuilder())
+                 .RegisterInstance(CoreSituationEncoding.CreateBuilder())
+                 .RegisterInstance(CruiseCommandEncoding.CreateBuilder())
+                 .RegisterInstance(FaultEncoding.CreateBuilder())
+                 .RegisterInstance(LightCommandEncoding.CreateBuilder())
+                 .RegisterInstance(LightSettingPacketEncoding.CreateBuilder())
+                 .RegisterInstance(LightStatePacetEncoding.CreateBuilder())
+                 .RegisterInstance(PedalConfigurationEncoding.CreateBuilder())
+                 .RegisterInstance(PedalSettingEncoding.CreateBuilder())
+                 .RegisterInstance(ServoInputEncoding.CreateBuilder())
+                 .RegisterInstance(ServoOutputEncoding.CreateBuilder())
+                 .RegisterInstance(ThrottleConfigurationEncoding.CreateBuilder())
+                 ;
+        }
+    }
+}
---
+++ MaterialDesignUnityBootStrap.SharpCommunication/SharpCommunicationModule.cs
@@ -0,0 +1,20 @@
+﻿using MaterialDesignUnityBootStrap.SharpCommunication.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace MaterialDesignUnityBootStrap.SharpCommunication
+{
+    public class SharpCommunicationModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
