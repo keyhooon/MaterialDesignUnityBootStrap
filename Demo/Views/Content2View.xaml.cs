e4a3de0357c13e1427dd70b043084a0c81bf9ef1
keyhanbabazadeh <keyhanbabazadeh@hotmail.com>
2021-04-21 19:40:00 +0430

    ...

--- Demo/Views/Content2View.xaml.cs
+++ Demo/Views/Content2View.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Demo.Views
 {
@@ -10,6 +12,26 @@ namespace Demo.Views
         public Content2View()
         {
             InitializeComponent();
+            // InitializeLoadingComponent();
+            // Task.Factory.StartNew(async () =>
+            // {
+            //     await Task.Delay(3000);
+            //     ((UIElement) this.Content).Visibility = Visibility.Collapsed;
+            //     InitializeComponent();
+            // });
         }
+        // public void InitializeLoadingComponent()
+        // {
+        //     if (_contentLoaded)
+        //     {
+        //         return;
+        //     }
+        //     _contentLoaded = true;
+        //     var resourceLocater = new System.Uri("Demo;V1.0.0.0;component/views/Loading.xaml", System.UriKind.Relative);
+        //     System.Windows.Application.LoadComponent(this, resourceLocater);
+        //
+        //
+        // }
+
     }
 }
